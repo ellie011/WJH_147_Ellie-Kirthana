4d067673accd228026f4b425860fd45ce7b20448
ellie011 <35493829+ellie011@users.noreply.github.com>
2018-03-06 10:06:53 -0600

    Add files via upload

---
+++ 1_4_7Omega1.py
@@ -0,0 +1,16 @@
+import PIL
+import matplotlib.pyplot as plt
+import os.path  
+
+directory = os.path.dirname(os.path.abspath(__file__))  
+phonecase_file = os.path.join(directory, '1.png')
+
+ 
+#phone_img.paste(logo_small, (611,876), mask=logo_small) 
+
+#fig3, axes3 = plt.subplots(1, 2)
+#axes3[0].imshow(phone_img, interpolation='none')
+#axes3[1].imshow(phone_img, interpolation='none')
+#axes3[1].set_xlim(500, 600)
+#axes3[1].set_ylim (900, 1000)
+#fig3.show() 
