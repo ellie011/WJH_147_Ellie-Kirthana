76ba7d14b97dbe369744b858947e85d36bf50a6c
ellie011 <35493829+ellie011@users.noreply.github.com>
2018-03-07 07:56:30 -0600

    Add files via upload

---
+++ 1_4_7Omega2.py
@@ -0,0 +1,39 @@
+import PIL
+import matplotlib.pyplot as plt
+import os.path  
+
+
+
+ 
+#phone_img.paste(logo_small, (611,876), mask=logo_small) 
+
+#fig3, axes3 = plt.subplots(1, 2)
+#axes3[0].imshow(phone_img, interpolation='none')
+#axes3[1].imshow(phone_img, interpolation='none')
+#axes3[1].set_xlim(500, 600)
+#axes3[1].set_ylim (900, 1000)
+#fig3.show() 
+
+
+
+#def logo_on_images():
+    
+    #if directory == None:
+       # directory = os.getcwd()
+        
+   # new_directory = os.path.join(directory, 'modified')
+    #try:
+      #  os.mkdir(new_directory)
+   # except OSError:
+     #   pass
+    
+   # image_list, file_list = get_images(directory)  
+
+    #for n in range(len(image_list)):
+       # filename, filetype = file_list[n].split('.')
+                
+       # framed_image = frame_image(image_list[n], (127, 0, 127), thickness)
+        
+       # framed_image_filename = os.path.join(new_directory, filename + '.png')
+       # framed_image.save(framed_image_filename)
+        
